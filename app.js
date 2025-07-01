689988a499b4c9ed5af5e543c72b0a91c0c8ac03
Dk-variya007 <variyadivyesh79@gmail.com>
2025-07-01 22:46:13 +0530

    new

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const port = process.env.PORT || 3000;
   Cloudinary Configuration
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━*/
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env?.CLOUDINARY_CLOUD_NAME?.trim(),
+  api_key: process.env?.CLOUDINARY_API_KEY?.trim(),
+  api_secret: process.env?.CLOUDINARY_API_SECRET?.trim(),
 });
 
 /*━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
